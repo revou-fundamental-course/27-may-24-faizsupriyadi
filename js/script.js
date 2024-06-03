ce27d5452fa30028fe3c1b873239518f7f08f025
faiz <faizsupriyadi1@gmail.com>
2024-06-03 12:22:31 +0700

    hmm

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-// ini script js
 document.addEventListener("DOMContentLoaded", function () {
+  // Prompt untuk nama viewer saat halaman dimuat
+  const viewerNameSpan = document.getElementById("viewer-name");
+  const viewerName = prompt("Masukan nama anda:");
+
+  if (viewerName) {
+    viewerNameSpan.textContent = viewerName;
+  }
+
+  // Form handling dan menampilkan hasil input form
   const form = document.getElementById("userForm");
-  const viewerName = document.getElementById("viewer-name");
   const outputName = document.getElementById("outputName");
   const outputDob = document.getElementById("outputDob");
   const outputGender = document.getElementById("outputGender");
   const outputMessage = document.getElementById("outputMessage");
   const currentTime = document.getElementById("currentTime");
 
+  // Fungsi untuk memperbarui waktu saat ini
   function updateCurrentTime() {
     const now = new Date();
     const options = {
@@ -23,18 +31,25 @@ document.addEventListener("DOMContentLoaded", function () {
     currentTime.textContent = `Current Time: ${formattedTime}`;
   }
 
+  // Event listener untuk submit form
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const name = document.getElementById("name").value;
-    const dob = document.getElementById("dob").value;
+    const dob = new Date(document.getElementById("dob").value);
+    const formattedDob = dob.toLocaleDateString("id-ID", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
     const gender =
       document.querySelector('input[name="gender"]:checked')?.value || "";
     const message = document.getElementById("message").value;
 
-    viewerName.textContent = name;
+    // Update hasil input form
+    viewerNameSpan.textContent = name;
     outputName.textContent = name;
-    outputDob.textContent = dob;
+    outputDob.textContent = formattedDob;
     outputGender.textContent = gender;
     outputMessage.textContent = message;
 
